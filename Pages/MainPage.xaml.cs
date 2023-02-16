71e346ff06af5056c73fe7779635f43417ba2b8e
Вячеслав Находнов <slava.nah91@gmail.com>
2023-02-16 15:48:52 +0300

    Добавьте файлы проекта.

---
+++ Pages/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Hakaton.ClassHelper;
+
+namespace Hakaton.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnIStudent_Click(object sender, RoutedEventArgs e)
+        {
+            ClassHelper.NavigateClass.navigFrame.Navigate(new EnterPage());
+            
+        }
+
+        private void btnIApplicant_Click(object sender, RoutedEventArgs e)
+        {
+            ClassHelper.NavigateClass.navigFrame.Navigate(new StudentsCatalog());
+        }
+    }
+}
